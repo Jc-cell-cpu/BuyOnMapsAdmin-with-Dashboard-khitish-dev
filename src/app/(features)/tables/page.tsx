8e82cddb12f2005ab2d1084d6154dd0421bf98c9
Jc-cell-cpu <sdmohapatra2000@gmail.com>
2025-04-16 16:33:53 +0530

    refactor: uncomment metadata and layout imports in Tables page

--- src/app/(features)/tables/page.tsx
+++ src/app/(features)/tables/page.tsx
@@ -3,26 +3,26 @@
 // // import TableThree from "@/components/UserTable/UserList";
 // // import TableTwo from "@/components/Tables/TableTwo";
 
-// import { Metadata } from "next";
-// import DefaultLayout from "@/components/Layouts/DefaultLaout";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLaout";
 
-// export const metadata: Metadata = {
-//   title: "Next.js Tables Page | NextAdmin - Next.js Dashboard Kit",
-//   description: "This is Next.js Tables page for NextAdmin Dashboard Kit",
-// };
+export const metadata: Metadata = {
+  title: "Next.js Tables Page | NextAdmin - Next.js Dashboard Kit",
+  description: "This is Next.js Tables page for NextAdmin Dashboard Kit",
+};
 
-// const TablesPage = () => {
-//   return (
-//     <DefaultLayout>
-//       {/* <Breadcrumb pageName="Tables" /> */}
+const TablesPage = () => {
+  return (
+    <DefaultLayout>
+      {/* <Breadcrumb pageName="Tables" /> */}
 
-//       <div className="flex flex-col gap-10">
-//         {/* <TableOne /> */}
-//         {/* <TableTwo /> */}
-//         {/* <TableThree /> */}
-//       </div>
-//     </DefaultLayout>
-//   );
-// };
+      <div className="flex flex-col gap-10">
+        {/* <TableOne /> */}
+        {/* <TableTwo /> */}
+        {/* <TableThree /> */}
+      </div>
+    </DefaultLayout>
+  );
+};
 
-// export default TablesPage;
+export default TablesPage;
